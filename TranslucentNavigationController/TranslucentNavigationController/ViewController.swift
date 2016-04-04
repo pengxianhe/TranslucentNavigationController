c32310da86bfc7253749d586cbf2f36fd96b8d2b
彭显鹤 <pengxianhe@126.com>
2016-04-04 17:50:37 +0800

    创建项目

---
+++ TranslucentNavigationController/TranslucentNavigationController/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TranslucentNavigationController
+//
+//  Created by 彭显鹤 on 16/4/4.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.barTintColor
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
